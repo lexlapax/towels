ab4a99404b21ae24f93e2748feef1bac94963e7a
Lex Lapax <lexlapax@gmail.com>
2022-10-16 22:34:43 -0700

    playing around with views

--- towels_gui/src/main.rs
+++ towels_gui/src/main.rs
@@ -1,34 +1,10 @@
-#![warn(clippy::all, rust_2018_idioms)]
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+pub(crate) use crate::gui::*;
+mod gui;
 
-// When compiling natively:
-#[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    // Log to stdout (if you run with `RUST_LOG=debug`).
-    tracing_subscriber::fmt::init();
-
-    let native_options = eframe::NativeOptions::default();
-    eframe::run_native(
-        env!("CARGO_PKG_DESCRIPTION"),
-        native_options,
-        Box::new(|cc| Box::new(towels_gui::TemplateApp::new(cc))),
-    );
+    nannou::app(model)
+        .update(update)
+        .simple_window(view)
+        .run();
 }
 
-// // when compiling to web using trunk.
-// #[cfg(target_arch = "wasm32")]
-// fn main() {
-//     // Make sure panics are logged using `console.error`.
-//     console_error_panic_hook::set_once();
-
-//     // Redirect tracing to console.log and friends:
-//     tracing_wasm::set_as_global_default();
-
-//     let web_options = eframe::WebOptions::default();
-//     eframe::start_web(
-//         "the_canvas_id", // hardcode it
-//         web_options,
-//         Box::new(|cc| Box::new(towels_gui::TemplateApp::new(cc))),
-//     )
-//     .expect("failed to start eframe");
-// }
